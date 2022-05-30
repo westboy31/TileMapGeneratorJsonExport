c5d45be298412156e5e97c1f9e31120a8e095a3c
westboy31 <daliyoucefmedakram@gmail.com>
2022-05-30 22:44:13 +0100

    Add files via upload

--- Godot @westboy31/TileMapGenerator.gd
+++ Godot @westboy31/TileMapGenerator.gd
@@ -4,6 +4,8 @@ class_name TileMapGenerator,"tmg_icon.png"
 var tmg_File = File.new()
 var tmg_Json
 var items = {}
+var tmg_collider = null
+
 
 export var tmg_jsonPath = ""
 export var tmg_itemFolder = ""
@@ -18,28 +20,40 @@ func tmg_GetJson(jsonPath) :
 
 
 func _ready():
-	
+
 	tmg_GetJson(tmg_jsonPath)
-	
+
 	for i in tile_set.get_tiles_ids():
 		tile_set.tile_set_name(i, tile_set.tile_get_name(i).split(".",true,1)[0])
 		pass
-	
+
 	for i in tmg_Json.ItemsList :
 		items[i.split(".",true,1)[0]] = load(tmg_itemFolder + "/" + i.split(".",true,1)[0]+".tscn")
 		pass
 
+	if tmg_Json.Map == "PlatformerHorizontal" or tmg_Json.Map == "PlatformerMultidirectional" :
+		tmg_collider = load("TileMapGeneratorCollider.tscn")
+		pass
+
 	for tile in tmg_Json.Tiles :
 		set_cell(tile.X ,tile.Y ,tile_set.find_tile_by_name(tile.Tile),tile.FlipHorizontally,tile.FlipVertically)
-	
+
 	for i in tmg_Json.Items :
 		var item = items[i.Item.split(".",true,1)[0]].instance()
 		item.position = Vector2(i.X,i.Y)
+		item.centered = false
 		add_child(item)
-		pass 
+		pass
+
+	if tmg_collider != null :
+		for collider in tmg_Json.Collisions :
+			var collision = tmg_collider.instance()
+			collision.scale.x *= collider.Width
+			collision.scale.y *= collider.Height
+			collision.position.x = collider.X + collider.Width/2
+			collision.position.y = collider.Y + collider.Height/2
+			add_child(collision)
+			pass
+		pass
 pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
